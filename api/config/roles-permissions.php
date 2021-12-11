1be6df4c2ab8ec33eb98c75ad3f4e240b110a784
ShokirjonMK <57320165+ShokirjonMK@users.noreply.github.com>
2021-12-11 16:44:09 +0500

    student access

--- api/config/roles-permissions.php
+++ api/config/roles-permissions.php
@@ -5,21 +5,72 @@ return [
     'student' => [
         'password_index',
         'password_update',
-        'time-table_index',
-        'time-table_view',
+        'faculty_view',
+        'direction_view',
+        'subject_view',
+        'languages_view',
+        'building_view',
+        'room_view',
+        'kafedra_view',
+        'para_view',
+        'edu-year_view',
+        'subject-category_view',
+        'exams-type_view',
+        'edu-type_view',
+        'edu-plan_view',
+        'edu-semestr_index',
+        'edu-semestr_view',
         'edu-semestr-subject_index',
         'edu-semestr-subject_view',
+        'edu-semestr-subject-category-time_view',
+
+        'time-table_index',
+        'time-table_view',
+        'week_index',
+        'week_view',
+        'region_view',
+        'area_view',
+        'country_view',
+        'semestr_index',
+        'semestr_view',
+        'course_index',
+        'course_view',
+        'subject-type_index',
+        'subject-type_view',
 
         'student-time-table_index',
         'student-time-table_create',
         'student-time-table_update',
         'student-time-table_view',
         'student-time-table_delete',
-        'semestr_index',
-        'semestr_view',
-        'edu-semestr_index',
-        'edu-semestr_view',
+
+        'student-exam_index',
+        'student-exam_create',
+        'student-exam_update',
+        'student-exam_view',
+        'student-exam_delete',
+
+        'exam-student-answer_index',
+        'exam-student-answer_create',
+        'exam-student-answer_update',
+        'exam-student-answer_view',
+        'exam-student-answer_delete',
+
+
+    ],
+
+    'teacher' => [
+        'password_index',
+        'password_update',
+        'exam-teacher-check_index',
+        'exam-teacher-check_update',
+
         
+        'subject-topic_index',
+        'subject-topic_create',
+        'subject-topic_update',
+        'subject-topic_view',
+        'subject-topic_delete',
     ],
 
     'chair_admin' => [
@@ -30,16 +81,6 @@ return [
         'subject_update',
         'subject_view',
         'subject_delete',
-    ],
-
-    'teacher' => [
-        'password_index',
-        'password_update',
-        'subject-topic_index',
-        'subject-topic_create',
-        'subject-topic_update',
-        'subject-topic_view',
-        'subject-topic_delete',
     ],
 
     'directory_editor' => [

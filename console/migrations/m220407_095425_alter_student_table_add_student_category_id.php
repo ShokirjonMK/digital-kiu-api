b0dc73a3b2aaba716070de5bb93a6a79e52178e9
ShokirjonMk <mkshokirjon@gmail.com>
2022-04-07 15:45:49 +0500

    Update m220407_095425_alter_student_table_add_student_category_id.php

--- console/migrations/m220407_095425_alter_student_table_add_student_category_id.php
+++ console/migrations/m220407_095425_alter_student_table_add_student_category_id.php
@@ -12,7 +12,7 @@ class m220407_095425_alter_student_table_add_student_category_id extends Migrati
      */
     public function safeUp()
     {
-        // $this->execute("ALTER TABLE `student` ADD `student_category_id` int NULL COMMENT 'student_category id ';");
+        $this->execute("ALTER TABLE `student` ADD `student_category_id` int NULL COMMENT 'student_category id ';");
 
         $this->addForeignKey('ssc_student_student_category_mk', 'student', 'student_category_id', 'student_category', 'id');
     }

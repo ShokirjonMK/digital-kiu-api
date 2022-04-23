3018eb98ed6ccb2e8c337599dcbc8df4825395cb
ShokirjonMk <mkshokirjon@gmail.com>
2022-04-23 11:10:08 +0500

    Update FacultyStatistic.php

--- common/models/model/FacultyStatistic.php
+++ common/models/model/FacultyStatistic.php
@@ -20,7 +20,7 @@ class FacultyStatistic extends Faculty
                 return $model->translate->name ?? '';
             },
             'studentsCount' => function ($model) {
-                return $model->studentsCount ?? [];
+                return $model->studentsCount ?? 0;
             },
 
         ];
@@ -36,6 +36,14 @@ class FacultyStatistic extends Faculty
             'genderMale',
             'genderFemale',
 
+
+            'teachers',
+            'teachersCount',
+
+
+            'description',
+            'createdBy',
+            'updatedBy',
         ];
 
         return $extraFields;
@@ -76,8 +84,32 @@ class FacultyStatistic extends Faculty
 
     public function getStudentsCount()
     {
-        // return   $this->studentsAll;
-
         return count($this->studentsAll);
+    }
+
+    public function getTeachers()
+    {
+        $model = new User();
+        $query = $model->find()
+            ->join(
+                'INNER JOIN',
+                'auth_assignment',
+                'auth_assignment.user_id = users.id'
+            )
+            ->join(
+                'INNER JOIN',
+                'user_access',
+                'user_access.user_id = users.id'
+            );
+
+        $query = $query->andWhere(['user_access.user_access_type_id' => self::USER_ACCESS_TYPE_ID]);
+        $query = $query->andWhere(['user_access.table_id' => $this->id]);
+        $query = $query->andWhere(['in', 'auth_assignment.item_name', ['teacher', 'mudir']]);
+        return $query->all();
+    }
+
+    public function getTeachersCount()
+    {
+        return count($this->teachers);
     }
 }

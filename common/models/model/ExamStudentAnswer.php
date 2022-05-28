063f97ca7ae967bae042cdcb22c560849d61f85b
ShokirjonMk <mkshokirjon@gmail.com>
2022-05-28 20:09:59 +0500

    Update ExamStudentAnswer.php

--- common/models/model/ExamStudentAnswer.php
+++ common/models/model/ExamStudentAnswer.php
@@ -191,7 +191,7 @@ class ExamStudentAnswer extends \yii\db\ActiveRecord
             'teacherAccess',
             'questionType',
 
-            //            'subQuestionAnswers',
+            //'subQuestionAnswers',
             'examStudentAnswerSubQuestion',
 
             'subQuestions',

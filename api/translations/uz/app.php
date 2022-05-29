2a3ccc26397c167240b7512d4ce466698677d2c2
ShokirjonMk <mkshokirjon@gmail.com>
2022-05-29 08:33:18 +0500

    Exam translate

--- api/translations/uz/app.php
+++ api/translations/uz/app.php
@@ -5,4 +5,18 @@
 
   'Incorrect password' => 'Parol noto\'g\'ri!',
   'Data not found.' => 'Ma\'lumot topilmadi!',
+  'Student not found' => 'Talab topilmadi!',
+  'Password required!'=> 'Parol talab etiladi!',
+  'Not Generated!' => 'Generatsiya qilinmagan!',
+  'The question is not specified' => 'Savol biriktirilmagan!',
+  'Questions are not found for this exam' => 'Bu imtihon uchun savol topilmadi!',
+  'This exam`s time is not starts' => 'Imtihon hali boshlanmadi',
+  'This exam`s time expired' => 'Imtihon vaqti tugagan!',
+  'Incorrect password' => 'Parol noto\'g\'ri kiritildi!',
+  'This exam not found' => 'Imtihon topilmadi!',
+  'Exam id required' => 'Imtihon ID majburiy!',
+  'This exam already finished for you!' => 'Siz uchun imtihon vaqti tugagan!',
+  'This eman is not for you!' => 'Bu imtihon siz uchun emas!',
+  
+
 ];

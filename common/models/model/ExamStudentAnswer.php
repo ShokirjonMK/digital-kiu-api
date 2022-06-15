1954d57fe33d90ede2d081271ec3936d6389191e
ShokirjonMk <mkshokirjon@gmail.com>
2022-06-15 12:08:03 +0500

    Update ExamStudentAnswer.php

--- common/models/model/ExamStudentAnswer.php
+++ common/models/model/ExamStudentAnswer.php
@@ -337,7 +337,7 @@ class ExamStudentAnswer extends \yii\db\ActiveRecord
                             $data['questions'] = $getQuestion;
                             if ($exam->id = 345 || $exam->id = 346) {
                                 $ExamStudentHas->start = time();
-                                $ExamStudentHas->save(false);
+                                $ExamStudentHas->save();
                             }
                             $exam_times['start'] = date("Y-m-d H:i:s", $ExamStudentHas->start);
                             $exam_times['duration'] = $exam->duration;

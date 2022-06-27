f37666c36b7c7c8ca2ac2eee7560130fb1fbccbc
ShokirjonMk <mkshokirjon@gmail.com>
2022-06-27 11:47:31 +0500

    Update Faculty.php

--- common/models/model/Faculty.php
+++ common/models/model/Faculty.php
@@ -195,7 +195,7 @@ class Faculty extends \yii\db\ActiveRecord
     public function getuserAccess()
     {
         return $this->hasMany(UserAccess::className(), ['table_id' => 'id'])
-            ->andOnCondition(['USER_ACCESS_TYPE_ID' => self::USER_ACCESS_TYPE_ID]);
+            ->andOnCondition(['USER_ACCESS_TYPE_ID' => self::USER_ACCESS_TYPE_ID, 'is_deleted' => 0]);
     }
 
     public static function createItem($model, $post)

a6bc2d127fbd8e0d6fc27d7794225aa585326ab6
ShokirjonMK <57320165+ShokirjonMK@users.noreply.github.com>
2022-07-19 23:14:06 +0500

    Update ApiActionTrait.php

--- api/controllers/ApiActionTrait.php
+++ api/controllers/ApiActionTrait.php
@@ -73,7 +73,7 @@ trait ApiActionTrait
      * @return void
      */
 
-    public function afterAction($action, $result)
+   /*  public function afterAction($action, $result)
     {
 
         // vdd(Yii::$app->request);
@@ -97,7 +97,7 @@ trait ApiActionTrait
 
         $result = parent::afterAction($action, $result);
         return $result;
-    }
+    } */
 
     /**
      * Before action
@@ -136,7 +136,7 @@ trait ApiActionTrait
             // vdd(Yii::$app->request->get());
             // vdd(Yii::$app->request->post());
 
-            $action_log = new ActionLog();
+         /*    $action_log = new ActionLog();
             $action_log->user_id = current_user_id();
             $action_log->controller = Yii::$app->controller->id;
             $action_log->action = Yii::$app->controller->action->id;
@@ -144,7 +144,7 @@ trait ApiActionTrait
             $action_log->get_data = json_encode(Yii::$app->request->get());
             $action_log->post_data = json_encode(Yii::$app->request->post());
             $action_log->save(false);
-            Yii::$app->session->set('action_log', $action_log);
+            Yii::$app->session->set('action_log', $action_log); */
 
             // dd(current_user_id());
             Yii::$app->language = $lang;

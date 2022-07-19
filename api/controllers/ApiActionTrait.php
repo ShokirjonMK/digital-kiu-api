50190f5502253179d485130cf9f4370bb47cd183
ShokirjonMK <57320165+ShokirjonMK@users.noreply.github.com>
2022-07-19 09:46:40 +0500

    Update ApiActionTrait.php

--- api/controllers/ApiActionTrait.php
+++ api/controllers/ApiActionTrait.php
@@ -85,7 +85,7 @@ trait ApiActionTrait
         $action_log->user_id = current_user_id();
         $action_log->status = isset($result['status']) ? $result['status'] : 'Failed';
         $action_log->message = isset($result['message']) ? $result['message'] : 'Failed';
-        // $action_log->browser = json_encode(getBrowser());
+        $action_log->browser = json_encode(getBrowser());
         // $action_log->ip_address = getIpAddress();
         // $action_log->host = get_host();
         // $action_log->ip_address_data = json_encode(getIpAddressData());

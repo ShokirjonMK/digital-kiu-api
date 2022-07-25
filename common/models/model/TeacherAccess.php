072c0b30c6e0e9b3fc9126a796153788fc656737
ShokirjonMk <mkshokirjon@gmail.com>
2022-07-25 15:23:52 +0500

    Update TeacherAccess.php

--- common/models/model/TeacherAccess.php
+++ common/models/model/TeacherAccess.php
@@ -170,7 +170,7 @@ class TeacherAccess extends \yii\db\ActiveRecord
      */
     public function getTeacher()
     {
-        return $this->hasOne(Profile::className(), ['user_id' => 'user_id'])->select(['first_name', 'last_name', 'middle_name']);
+        return $this->hasOne(Profile::className(), ['user_id' => 'user_id']); //->select(['first_name', 'last_name', 'middle_name']);
     }
 
     /**

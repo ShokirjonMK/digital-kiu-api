82339eaebdd04e6b602f8c7274d68a283115d047
ShokirjonMk <mkshokirjon@gmail.com>
2022-08-08 09:35:27 +0500

    Update SubjectContentController.php

--- api/controllers/SubjectContentController.php
+++ api/controllers/SubjectContentController.php
@@ -35,7 +35,11 @@ class SubjectContentController extends ApiActiveController
         if (isRole('teacher') && !isRole('mudir')) {
             $query->andWhere([$this->table_name . '.created_by' => current_user_id()]);
         }
-        
+
+        if (Yii::$app->request->get('user_id') != null) {
+            $query->andWhere([$this->table_name . '.created_by' => Yii::$app->request->get('sort')]);
+        }
+
         // filter
         $query = $this->filterAll($query, $model);
 

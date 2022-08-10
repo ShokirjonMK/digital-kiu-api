2c1300b5d5313582c05f4ff51ef39c850c42fa45
ShokirjonMk <mkshokirjon@gmail.com>
2022-08-10 11:25:52 +0500

    Update roles-permissions.php

--- api/config/roles-permissions.php
+++ api/config/roles-permissions.php
@@ -1364,7 +1364,7 @@ return [
         "exam_view",
 
         'exam-appeal_view',
-        // "exam-appeal_create",
+        "exam-appeal_create",
         // 'exam-appeal_delete',
         // 'exam-appeal_update',
 

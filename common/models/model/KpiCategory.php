b0349e5e98ceeaa6a0178e071c7c51c219fbc28c
ShokirjonMk <mkshokirjon@gmail.com>
2022-08-12 16:46:59 +0500

    Update KpiCategory.php

--- common/models/model/KpiCategory.php
+++ common/models/model/KpiCategory.php
@@ -195,7 +195,8 @@ class KpiCategory extends \yii\db\ActiveRecord
     public function getKpiData()
     {
         // return 1;
-        return $this->hasMany(KpiData::className(), ['kpi_category_id'=> 'id']); //->onCondition(['user_id' => Yii::$app->request->get('user_id') ?? current_user_id()]);
+
+        return $this->hasMany(KpiData::className(), ['kpi_category_id' => 'id'])->onCondition(['user_id' => Yii::$app->request->get('user_id') ?? current_user_id()]);
     }
 
 

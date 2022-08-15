d90cbe14f7d32f7555dc3135a020f188020bcd50
ShokirjonMk <mkshokirjon@gmail.com>
2022-08-15 09:45:48 +0500

    Update LangCertificateController.php

--- api/controllers/LangCertificateController.php
+++ api/controllers/LangCertificateController.php
@@ -84,16 +84,22 @@ class LangCertificateController extends ApiActiveController
         return $this->response(1, _e('Success.'), $model, null, ResponseStatus::OK);
     }
 
-
     public function actionDelete($lang, $id)
     {
         $model = LangCertificate::find()
             ->andWhere(['id' => $id, 'is_deleted' => 0])
             ->one();
-        $model->delete();
+
         if (!$model) {
             return $this->response(0, _e('Data not found.'), null, null, ResponseStatus::NOT_FOUND);
         }
-        return $this->response(1, _e('Success.'), $model, null, ResponseStatus::OK);
+
+        if ($model) {
+            $model->is_deleted = 1;
+            $model->update();
+
+            return $this->response(1, _e($this->controller_name . ' succesfully removed.'), null, null, ResponseStatus::OK);
+        }
+        return $this->response(0, _e('There is an error occurred while processing.'), null, null, ResponseStatus::BAD_REQUEST);
     }
 }

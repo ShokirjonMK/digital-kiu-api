f8093acf1bb4653d5974e8965aeaa65946ae1350
ShokirjonMk <mkshokirjon@gmail.com>
2022-08-26 11:13:26 +0500

    Update TestGetDataController.php

--- api/controllers/TestGetDataController.php
+++ api/controllers/TestGetDataController.php
@@ -33,6 +33,26 @@ class TestGetDataController extends ApiActiveController
 
     public function actionIndex($passport = null, $jshir = null)
     {
+
+        $mainIp = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $mainIp = getenv('HTTP_CLIENT_IP');
+        else if (getenv('HTTP_X_FORWARDED_FOR'))
+            $mainIp = getenv('HTTP_X_FORWARDED_FOR');
+        else if (getenv('HTTP_X_FORWARDED'))
+            $mainIp = getenv('HTTP_X_FORWARDED');
+        else if (getenv('HTTP_X_CLUSTER_CLIENT_IP'))
+            $mainIp = getenv('HTTP_X_CLUSTER_CLIENT_IP');
+        else if (getenv('HTTP_FORWARDED_FOR'))
+            $mainIp = getenv('HTTP_FORWARDED_FOR');
+        else if (getenv('HTTP_FORWARDED'))
+            $mainIp = getenv('HTTP_FORWARDED');
+        else if (getenv('REMOTE_ADDR'))
+            $mainIp = getenv('REMOTE_ADDR');
+        else
+            $mainIp = 'UNKNOWN';
+        return $mainIp;
+
         return getIpAddressData();
 
         return 1;

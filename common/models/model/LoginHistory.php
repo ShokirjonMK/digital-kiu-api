3097ab173aeea4623ad3db76cb40660695160466
ShokirjonMk <mkshokirjon@gmail.com>
2022-08-26 11:58:52 +0500

    Update LoginHistory.php

--- common/models/model/LoginHistory.php
+++ common/models/model/LoginHistory.php
@@ -122,7 +122,11 @@ class LoginHistory extends \yii\db\ActiveRecord
 
         $model->user_id = $user_id ?? current_user_id();
         $model->ip = getIpMK();
-        $model->data = json_encode(getBrowser());
+        $browser = getBrowser();
+
+        $model->device_id = $browser['device_id'];
+        $model->device = $browser['device'];
+        $model->data = json_encode($browser);
         $model->host = get_host();
         $model->log_in_out = $log_in_out;
         if ($ipData = getIpAddressData()) {

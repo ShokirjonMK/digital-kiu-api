56a4f24a29fad55eda03777cdc1305adbbd4f69a
ShokirjonMk <mkshokirjon@gmail.com>
2022-08-26 11:21:02 +0500

    Get Real ip address

--- base/helpers/tools.php
+++ base/helpers/tools.php
@@ -419,11 +419,35 @@ function getIpAddress()
     return \Yii::$app->request->getUserIP();
 }
 
+
+// Get IP address
+function getIpMK()
+{
+    $mainIp = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $mainIp = getenv('HTTP_CLIENT_IP');
+    else if (getenv('HTTP_X_FORWARDED_FOR'))
+        $mainIp = getenv('HTTP_X_FORWARDED_FOR');
+    else if (getenv('HTTP_X_FORWARDED'))
+        $mainIp = getenv('HTTP_X_FORWARDED');
+    else if (getenv('HTTP_X_CLUSTER_CLIENT_IP'))
+        $mainIp = getenv('HTTP_X_CLUSTER_CLIENT_IP');
+    else if (getenv('HTTP_FORWARDED_FOR'))
+        $mainIp = getenv('HTTP_FORWARDED_FOR');
+    else if (getenv('HTTP_FORWARDED'))
+        $mainIp = getenv('HTTP_FORWARDED');
+    else if (getenv('REMOTE_ADDR'))
+        $mainIp = getenv('REMOTE_ADDR');
+    else
+        $mainIp = 'UNKNOWN';
+    return $mainIp;
+}
+
 // Get IP address data
 function getIpAddressData($ip_address = null)
 {
     if (is_null($ip_address)) {
-        $ip_address = \Yii::$app->request->userIP;
+        $ip_address = getIpMK();
     }
 
     if ($ip_address != '127.0.0.1') {

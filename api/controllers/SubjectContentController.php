9449d7d6c5439148562e7fe561a2c62c17170932
ShokirjonMk <mkshokirjon@gmail.com>
2022-09-07 11:37:35 +0500

    Update SubjectContentController.php

--- api/controllers/SubjectContentController.php
+++ api/controllers/SubjectContentController.php
@@ -58,7 +58,7 @@ class SubjectContentController extends ApiActiveController
         $query = $model->find()
             ->andWhere([$this->table_name . '.is_deleted' => 0]);
 
-        if (isRole('teacher') && (!isRole('mudir') || !isRole('contenter'))) {
+        if (isRole('teacher') && (!isRole('mudir') && !isRole('contenter'))) {
             // if (isRole('teacher') && !isRole('mudir')) {
             $query->andWhere([$this->table_name . '.created_by' => current_user_id()]);
         }

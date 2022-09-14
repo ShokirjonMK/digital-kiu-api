24c3e782bbec2a43f2364a055affd4215279c886
ShokirjonMk <mkshokirjon@gmail.com>
2022-09-14 17:47:35 +0500

    Time Option CRUD

---
+++ api/controllers/TimeOptionController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace api\controllers;
+
+use common\models\model\TimeOption;
+use Yii;
+use base\ResponseStatus;
+use common\models\model\Faculty;
+
+class TimeOptionController extends ApiActiveController
+{
+    public $modelClass = 'api\resources\TimeOption';
+
+    public function actions()
+    {
+        return [];
+    }
+
+    public function actionIndex($lang)
+    {
+        $model = new TimeOption();
+
+        $query = $model->find()
+            ->andWhere(['is_deleted' => 0]);
+
+        $student = $this->student(2);
+        if ($student) {
+            $query->andWhere(['language_id' => $student->edu_lang_id]);
+            $query->andWhere(['edu_plan_id' => $student->edu_plan_id]);
+        } else {
+            /*  is Self  */
+            $t = $this->isSelf(Faculty::USER_ACCESS_TYPE_ID);
+            if ($t['status'] == 1) {
+                $query->andFilterWhere([
+                    'faculty_id' => $t['UserAccess']->table_id
+                ]);
+            } elseif ($t['status'] == 2) {
+                $query->andFilterWhere([
+                    'faculty_id' => -1
+                ]);
+            }
+            /*  is Self  */
+        }
+
+        // filter
+        $query = $this->filterAll($query, $model);
+
+        // sort
+        $query = $this->sort($query);
+
+        // data
+        $data =  $this->getData($query);
+
+        return $this->response(1, _e('Success'), $data);
+    }
+
+    public function actionCreate($lang)
+    {
+        $model = new TimeOption();
+        $post = Yii::$app->request->post();
+        $this->load($model, $post);
+        $result = TimeOption::createItem($model, $post);
+        if (!is_array($result)) {
+            return $this->response(1, _e('TimeOption successfully created.'), $model, null, ResponseStatus::CREATED);
+        } else {
+            return $this->response(0, _e('There is an error occurred while processing.'), null, $result, ResponseStatus::UPROCESSABLE_ENTITY);
+        }
+    }
+
+    public function actionUpdate($lang, $id)
+    {
+        $model = TimeOption::findOne($id);
+        if (!$model) {
+            return $this->response(0, _e('Data not found.'), null, null, ResponseStatus::NOT_FOUND);
+        }
+        $post = Yii::$app->request->post();
+        $this->load($model, $post);
+        $result = TimeOption::updateItem($model, $post);
+        if (!is_array($result)) {
+            return $this->response(1, _e('TimeOption successfully updated.'), $model, null, ResponseStatus::OK);
+        } else {
+            return $this->response(0, _e('There is an error occurred while processing.'), null, $result, ResponseStatus::UPROCESSABLE_ENTITY);
+        }
+    }
+
+    public function actionView($lang, $id)
+    {
+        $model = TimeOption::find()
+            ->andWhere(['id' => $id])
+            ->one();
+        if (!$model) {
+            return $this->response(0, _e('Data not found.'), null, null, ResponseStatus::NOT_FOUND);
+        }
+        return $this->response(1, _e('Success.'), $model, null, ResponseStatus::OK);
+    }
+
+    public function actionDelete($lang, $id)
+    {
+        $model = TimeOption::findOne($id);
+        if (!$model) {
+            return $this->response(0, _e('Data not found.'), null, null, ResponseStatus::NOT_FOUND);
+        }
+
+        // remove model
+        $result = TimeOption::findOne($id);
+
+        if ($result) {
+            $result->delete();
+
+            return $this->response(1, _e('TimeOption succesfully removed.'), null, null, ResponseStatus::OK);
+        }
+        return $this->response(0, _e('There is an error occurred while processing.'), null, null, ResponseStatus::BAD_REQUEST);
+    }
+}
---
+++ common/models/model/TimeOption.php
@@ -0,0 +1,296 @@
+<?php
+
+namespace common\models\model;
+
+use api\resources\ResourceTrait;
+use Yii;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * This is the model class for table "time_table".
+ *
+ * @property int $id
+ * @property string $key
+ * @property int $faculty_id
+ * @property int $edu_plan_id
+ * @property int $edu_year_id
+ * @property int $edu_semester_id
+ * @property int $lang_id
+ * @property int $type
+ * @property string $description
+ * @property int|null $order
+ * @property int|null $status
+ * @property int $created_at
+ * @property int $updated_at
+ * @property int $created_by
+ * @property int $updated_by
+ * @property int $is_deleted
+ *
+ * @property Faculty_id $faculty_id
+ * @property Edu_plan_id $edu_plan_id
+ * @property Edu_year_id $edu_year_id
+ * @property Edu_semester_id $edu_semester_id
+ * @property Lang_id $lang_id
+ */
+class TimeOption extends \yii\db\ActiveRecord
+{
+    use ResourceTrait;
+
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::class,
+        ];
+    }
+
+    const STATUS_NEW = 1;
+    const STATUS_INACTIVE = 0;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'time_option';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [
+                [
+                    'key',
+                    // 'faculty_id',
+                    // 'edu_plan_id',
+                    // 'edu_year_id',
+                    'edu_semester_id',
+                    'language_id',
+                ], 'required'
+            ],
+            [
+                [
+                    'faculty_id',
+                    'edu_plan_id',
+                    'edu_year_id',
+                    'edu_semester_id',
+                    'language_id',
+
+                    // 'order',
+                    'status',
+                    'created_at',
+                    'updated_at',
+                    'created_by',
+                    'updated_by',
+                    'is_deleted'
+                ], 'integer'
+            ],
+            [['description'], 'string'],
+            [['key'], 'string', 'max' => 1],
+            ['key', 'filter', 'filter' => 'ucfirst'],
+            [
+                ['edu_semester_id'], 'exist',
+                'skipOnError' => true, 'targetClass' => EduSemestr::className(), 'targetAttribute' => ['edu_semester_id' => 'id']
+            ],
+            [
+                ['edu_year_id'], 'exist',
+                'skipOnError' => true, 'targetClass' => EduYear::className(), 'targetAttribute' => ['edu_year_id' => 'id']
+            ],
+            [
+                ['language_id'], 'exist',
+                'skipOnError' => true, 'targetClass' => Languages::className(), 'targetAttribute' => ['language_id' => 'id']
+            ],
+
+            [['key'], 'unique', 'targetAttribute' => ['key', 'edu_semester_id', 'is_deleted', 'language_id']],
+
+
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'edu_plan_id' => 'edu_plan_id',
+            'edu_year_id' => 'Edu Year ID',
+            'edu_semester_id' => 'Edu Semester ID',
+            'language_id' => 'Languages ID',
+
+            'key',
+            'faculty_id',
+            'type',
+            'description',
+
+            // 'order' => _e('Order'),
+            'status' => _e('Status'),
+            'created_at' => _e('Created At'),
+            'updated_at' => _e('Updated At'),
+            'created_by' => _e('Created By'),
+            'updated_by' => _e('Updated By'),
+            'is_deleted' => _e('Is Deleted'),
+        ];
+    }
+
+    public function fields()
+    {
+        $fields =  [
+            'id',
+
+            'name' => function ($model) {
+                return $model->key . '(' . $this->language->lang_code . ')';
+            },
+
+            'key',
+            'faculty_id',
+            'edu_plan_id',
+            'edu_year_id',
+            'edu_semester_id',
+            'language_id',
+            'type',
+            'description',
+
+            'status',
+            'created_at',
+            'updated_at',
+            'created_by',
+            'updated_by',
+
+        ];
+
+        return $fields;
+    }
+
+
+    public function extraFields()
+    {
+        $extraFields =  [
+
+            'eduYear',
+            'eduPlan',
+            'faculty',
+            'eduSemester',
+            'language',
+
+            'createdBy',
+            'updatedBy',
+            'createdAt',
+            'updatedAt',
+        ];
+
+        return $extraFields;
+    }
+
+
+    /**
+     * Gets query for [[faculty]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFaculty()
+    {
+        return $this->hasOne(Faculty::className(), ['id' => 'faculty_id']);
+    }
+    /**
+     * Gets query for [[EduYear]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEduYear()
+    {
+        return $this->hasOne(EduYear::className(), ['id' => 'edu_year_id']);
+    }
+
+    public function getEduPlan()
+    {
+        return $this->hasOne(EduPlan::className(), ['id' => 'edu_plan_id']);
+    }
+    public function getEduSemester()
+    {
+        return $this->hasOne(EduSemestr::className(), ['id' => 'edu_semester_id']);
+    }
+
+    /**
+     * Gets query for [[Language]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getLanguage()
+    {
+        return $this->hasOne(Languages::className(), ['id' => 'language_id'])->select(['name', 'lang_code']);
+    }
+
+    public static function createItem($model, $post)
+    {
+        $transaction = Yii::$app->db->beginTransaction();
+        $errors = [];
+
+        if (!($model->validate())) {
+            $errors[] = $model->errors;
+            $transaction->rollBack();
+            return simplify_errors($errors);
+        }
+
+        $model->edu_year_id = $model->eduSemester->edu_year_id;
+        $model->edu_plan_id = $model->eduSemester->edu_plan_id;
+        $model->faculty_id = $model->eduPlan->faculty_id;
+
+        if (!($model->validate())) {
+            $errors[] = $model->errors;
+            $transaction->rollBack();
+            return simplify_errors($errors);
+        }
+
+        if ($model->save()) {
+            $transaction->commit();
+            return true;
+        } else {
+            $transaction->rollBack();
+            return simplify_errors($errors);
+        }
+    }
+
+    public static function updateItem($model, $post)
+    {
+        $transaction = Yii::$app->db->beginTransaction();
+        $errors = [];
+
+        if (!($model->validate())) {
+            $errors[] = $model->errors;
+            $transaction->rollBack();
+            return simplify_errors($errors);
+        }
+
+        $model->edu_year_id = $model->eduSemester->edu_year_id;
+        $model->edu_plan_id = $model->eduSemester->edu_plan_id;
+        $model->faculty_id = $model->eduPlan->faculty_id;
+
+        if (!($model->validate())) {
+            $errors[] = $model->errors;
+            $transaction->rollBack();
+            return simplify_errors($errors);
+        }
+
+        if ($model->save()) {
+            $transaction->commit();
+            return true;
+        } else {
+            $transaction->rollBack();
+            return simplify_errors($errors);
+        }
+    }
+
+    public function beforeSave($insert)
+    {
+        if ($insert) {
+            $this->created_by = Current_user_id();
+        } else {
+            $this->updated_by = Current_user_id();
+        }
+        return parent::beforeSave($insert);
+    }
+}

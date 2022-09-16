9a88cd6d7d3ad92a582ad6771b5a2ad249956340
ShokirjonMk <mkshokirjon@gmail.com>
2022-09-16 15:54:14 +0500

    Update roles-permissions.php

--- api/config/roles-permissions.php
+++ api/config/roles-permissions.php
@@ -1439,16 +1439,16 @@ return [
 
     ],
 
-    // 'time_table_plus' => [
+    'time_table_plus' => [
 
-    //     "time-table_create",
-    //     "time-table_delete",
-    //     "time-table_index",
-    //     "time-table_parent-null",
-    //     "time-table_update",
-    //     "time-table_view",
+        "time-table_create",
+        "time-table_delete",
+        "time-table_index",
+        "time-table_parent-null",
+        "time-table_update",
+        "time-table_view",
 
-    // ],
+    ],
 
     'content_assign' => [
 

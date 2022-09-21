bf2ab176683d0bd148d41d9dc491d5de5202a65e
ShokirjonMk <mkshokirjon@gmail.com>
2022-09-21 09:54:16 +0500

    Update roles-permissions.php

--- api/config/roles-permissions.php
+++ api/config/roles-permissions.php
@@ -1400,7 +1400,7 @@ return [
 
 
         "student-time-table_create",
-        // "student-time-table_delete",
+        "student-time-table_delete",
         "student-time-table_index",
         // "student-time-table_update",
         "student-time-table_view",

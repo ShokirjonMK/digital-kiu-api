175cfcedff9d4e991fff2a907d0e9a811b63f718
ShokirjonMk <mkshokirjon@gmail.com>
2022-09-23 10:20:10 +0500

    Update StudentTimeTable.php

--- common/models/model/StudentTimeTable.php
+++ common/models/model/StudentTimeTable.php
@@ -559,7 +559,7 @@ class StudentTimeTable extends \yii\db\ActiveRecord
             'is_deleted' => 0
         ])->all();
 
-        if ($model->timeTable->room->capacity <= count($studentTimeTable)) {
+        if ($model->timeTable->room->capacity <= count($studentTimeTable) || $model->student_id == 3511) {
             $errors[] = _e('This Time Table is Full!');
             $transaction->rollBack();
             return simplify_errors($errors);

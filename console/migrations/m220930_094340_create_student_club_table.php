65608b1266c6d5f0843dfc80fe6099bccdd2aa52
ShokirjonMk <mkshokirjon@gmail.com>
2022-09-30 15:05:14 +0500

    Create m220930_094340_create_student_club_table.php

---
+++ console/migrations/m220930_094340_create_student_club_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%student_club}}`.
+ */
+class m220930_094340_create_student_club_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableName = Yii::$app->db->tablePrefix . 'student_club';
+        if (!(Yii::$app->db->getTableSchema($tableName, true) === null)) {
+            $this->dropTable('student_club');
+        }
+        $this->createTable('{{%student_club}}', [
+            'id' => $this->primaryKey(),
+
+            'club_category_id' => $this->integer()->null(),
+            'club_time_id' => $this->integer()->notNull(),
+            'club_id' => $this->integer()->notNull(),
+            'student_id' => $this->integer()->notNull(),
+            'faculty_id' => $this->integer()->null(),
+            'edu_plan_id' => $this->integer()->null(),
+            'gender' => $this->tinyInteger(1)->defaultValue(1),
+
+
+            'status' => $this->tinyInteger(1)->defaultValue(0),
+            'order' => $this->tinyInteger(1)->defaultValue(1),
+            'created_at' => $this->integer()->Null(),
+            'updated_at' => $this->integer()->Null(),
+            'created_by' => $this->integer()->notNull()->defaultValue(0),
+            'updated_by' => $this->integer()->notNull()->defaultValue(0),
+            'is_deleted' => $this->tinyInteger()->notNull()->defaultValue(0),
+        ]);
+
+        $this->addForeignKey('ctcc_student_club_club_category_id', 'student_club', 'club_category_id', 'club_category', 'id');
+        $this->addForeignKey('ctct_student_club_club_time_id', 'student_club', 'club_time_id', 'club_time', 'id');
+        $this->addForeignKey('ctct_student_club_club_id', 'student_club', 'club_id', 'club', 'id');
+        $this->addForeignKey('cts_student_club_student_id', 'student_club', 'student_id', 'student', 'id');
+        $this->addForeignKey('ctf_student_club_faculty_id', 'student_club', 'faculty_id', 'faculty', 'id');
+        $this->addForeignKey('ctep_student_club_edu_plan_id', 'student_club', 'edu_plan_id', 'edu_plan', 'id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+
+        $this->dropForeignKey('ctcc_student_club_club_category_id', 'student_club');
+        $this->dropForeignKey('ctct_student_club_club_time_id', 'student_club');
+        $this->dropForeignKey('ctct_student_club_club_id', 'student_club');
+        $this->dropForeignKey('cts_student_club_student_id', 'student_club');
+        $this->dropForeignKey('ctf_student_club_faculty_id', 'student_club');
+        $this->dropForeignKey('ctep_student_club_edu_plan_id', 'student_club');
+        $this->dropTable('{{%student_club}}');
+    }
+}

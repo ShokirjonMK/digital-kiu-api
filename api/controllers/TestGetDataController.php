2649d69aafebb49ebb90b17e15418312c8f51fa8
ShokirjonMk <mkshokirjon@gmail.com>
2022-10-24 19:15:15 +0500

    Update TestGetDataController.php

--- api/controllers/TestGetDataController.php
+++ api/controllers/TestGetDataController.php
@@ -40,7 +40,7 @@ class TestGetDataController extends ApiActiveController
         $errors = [];
         $soni = $i * 500;
 
-        return $this->response(1, _e('Success'), $soni);
+        // return $this->response(1, _e('Success'), $soni);
 
         $attends = Attend::find()
             ->limit(500)->offset($soni)->all();

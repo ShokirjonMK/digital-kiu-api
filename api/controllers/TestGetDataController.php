a3fb5e286a028c2b7a1476574f68e8d9dc95a043
ShokirjonMk <mkshokirjon@gmail.com>
2022-11-23 12:24:27 +0500

    Update TestGetDataController.php

--- api/controllers/TestGetDataController.php
+++ api/controllers/TestGetDataController.php
@@ -148,7 +148,7 @@ class TestGetDataController extends ApiActiveController
     {
 
     
-        $dataFormTable = KuvondikMasofaviy::find()->limit(10)->offset(10)->all();
+        $dataFormTable = KuvondikMasofaviy::find()->limit(10)->offset(20)->all();
 
 
         foreach ($dataFormTable as $dataOne) {

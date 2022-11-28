96b0ec8ba787c59550fb5c12fc75c83022864efe
ShokirjonMk <mkshokirjon@gmail.com>
2022-11-28 09:45:42 +0500

    Update ExamControlStudentController.php

--- api/controllers/ExamControlStudentController.php
+++ api/controllers/ExamControlStudentController.php
@@ -26,7 +26,7 @@ class ExamControlStudentController extends ApiActiveController
         $model = new ExamControlStudent();
 
         $query = $model->find()
-            ->with(['infoRelation'])
+            // ->with(['infoRelation'])
             ->andWhere([$this->table_name . '.is_deleted' => 0])
             ->leftJoin("translate tr", "tr.model_id = $this->table_name.id and tr.table_name = '$this->table_name'")
             // ->groupBy($this->table_name . '.id')

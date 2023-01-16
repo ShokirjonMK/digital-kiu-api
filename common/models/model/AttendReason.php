db0b68839bb91102afa6396cba20b6af4189dcf4
ShokirjonMk <mkshokirjon@gmail.com>
2023-01-16 16:03:23 +0500

    Update AttendReason.php

--- common/models/model/AttendReason.php
+++ common/models/model/AttendReason.php
@@ -286,7 +286,7 @@ class AttendReason extends \yii\db\ActiveRecord
         $studentAttends = $studentAttends::find();
         $reasonStart = date("Y-m-d", strtotime($model->start));
         $reasonEnd = date("Y-m-d", strtotime($model->end));
-        
+
         $studentAttends = $studentAttends
             ->where(['student_id' => $model->student_id])
             ->andWhere(['>=', 'date', $reasonStart])
@@ -297,7 +297,7 @@ class AttendReason extends \yii\db\ActiveRecord
 
             $t = false;
             if ($studentAttend->date == date("Y-m-d", strtotime($model->start))) {
-                if (($studentAttend->timeTable->para->end_time >= date('H:i', strtotime($model->start)))) {
+                if (($studentAttend->timeTable->para->start_time >= date('H:i', strtotime($model->start)))) {
                     $t = true;
                 }
             } elseif ($studentAttend->date == date("Y-m-d", strtotime($model->end))) {

8c69fddbb503db3451b9f1f5cb28a49e9fa23438
ShokirjonMk <mkshokirjon@gmail.com>
2023-01-22 21:47:36 +0500

    Update EduSemestrSubjectController.php

--- api/controllers/EduSemestrSubjectController.php
+++ api/controllers/EduSemestrSubjectController.php
@@ -21,6 +21,8 @@ class EduSemestrSubjectController extends ApiActiveController
     {
         return [];
     }
+    public $table_name = 'edu_semestr_subject';
+    public $controller_name = 'EduSemestrSubject';
 
     public function actionIndex($lang)
     {

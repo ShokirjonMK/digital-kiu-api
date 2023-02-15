60bb52416115f5571279c76b6e5358d1614b8c25
ShokirjonMk <mkshokirjon@gmail.com>
2023-02-15 09:33:48 +0500

    Update ExamController.php

--- api/controllers/ExamController.php
+++ api/controllers/ExamController.php
@@ -94,7 +94,7 @@ class ExamController extends ApiActiveController
 
         if (current_user_id() == 5510) {
             $query = $query->andFilterWhere([
-                'in', $this->table_name . '.id', [51]
+                'in', $this->table_name . '.edu_semestr_subject_id', [569]
             ]);
         } elseif ($student && isRole('student')) {
             //            dd($student->edu_plan_id);

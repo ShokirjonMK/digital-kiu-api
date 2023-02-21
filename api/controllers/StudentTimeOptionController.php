ca619e3825e77f5b18f9429d3dece486eee1dcc4
ShokirjonMk <mkshokirjon@gmail.com>
2023-02-21 10:07:19 +0500

    asd

--- api/controllers/StudentTimeOptionController.php
+++ api/controllers/StudentTimeOptionController.php
@@ -146,6 +146,8 @@ class StudentTimeOptionController extends ApiActiveController
     {
         $model = StudentTimeOption::findOne(['id' => $id]);
         if (!$model) {
+            return $this->response(0, _e('Old Option can not be deleted.'), null, null, ResponseStatus::UPROCESSABLE_ENTITY);
+
             return $this->response(0, _e('Data not found.'), null, null, ResponseStatus::NOT_FOUND);
         }
 

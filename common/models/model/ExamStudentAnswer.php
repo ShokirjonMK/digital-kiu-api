9a48757999698e0d0245a51f34057dc987ffc6d1
ShokirjonMk <mkshokirjon@gmail.com>
2023-04-07 10:21:45 +0500

    Update ExamStudentAnswer.php

--- common/models/model/ExamStudentAnswer.php
+++ common/models/model/ExamStudentAnswer.php
@@ -629,12 +629,7 @@ class ExamStudentAnswer extends \yii\db\ActiveRecord
                     if ($examStudentAnswerSubQuestion) {
                         if ($examStudentAnswerSubQuestion->exam_student_answer_id == $model->id) {
                             $examStudentAnswerSubQuestion->teacher_conclusion = $subQuestionOneAnswerChecking->teacher_conclusion;
-                            if (!$examStudentAnswerSubQuestion->ball > 0) {
-                                $examStudentAnswerSubQuestion->ball = $subQuestionOneAnswerChecking->ball;
-                            } else {
-                                $errors[] = _e("Ball can not change");
-                            }
-
+                            $examStudentAnswerSubQuestion->ball = $subQuestionOneAnswerChecking->ball;
                             if (isset($subQuestionOneAnswerChecking->teacher_conclusion) && isset($subQuestionOneAnswerChecking->ball)) {
                                 $examStudentAnswerSubQuestion->is_cheked = 1;
                             }

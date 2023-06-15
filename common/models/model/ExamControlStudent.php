ad3030c7a065df8f3655eab758fbaa0bb4c1596c
ShokirjonMk <mkshokirjon@gmail.com>
2023-06-15 14:30:26 +0500

    Update ExamControlStudent.php

--- common/models/model/ExamControlStudent.php
+++ common/models/model/ExamControlStudent.php
@@ -684,7 +684,7 @@ class ExamControlStudent extends ActiveRecord
 
         // faqat 4- kurslar uchun
         if (
-            !in_array($model->edu_plan_id, [15, 22, 27, 56, 132, 131, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98, 97, 96, 95, 55])
+            !in_array($model->edu_plan_id, [20, 15, 22, 27, 56, 132, 131, 108, 107, 106, 105, 104, 103, 102, 101, 100, 99, 98, 97, 96, 95, 55])
             && !in_array(
                 $model->student->profile->passport_pin,
                 [

071cd5b60f4d31a500470aa0946d8281a218b501
ShokirjonMk <mkshokirjon@gmail.com>
2023-07-11 13:43:31 +0500

    Update CountryController.php

--- api/controllers/CountryController.php
+++ api/controllers/CountryController.php
@@ -24,7 +24,7 @@ class CountryController extends ApiActiveController
 
         $query = $model->find()
             ->with(['infoRelation'])
-            ->andWhere([$this->table_name . '.is_deleted' => 0])
+            // ->andWhere([$this->table_name . '.is_deleted' => 0])
             ->leftJoin("translate tr", "tr.model_id = $this->table_name.id and tr.table_name = '$this->table_name'")
             ->groupBy($this->table_name . '.id')
             ->andFilterWhere(['like', 'tr.name', Yii::$app->request->get('q')]);

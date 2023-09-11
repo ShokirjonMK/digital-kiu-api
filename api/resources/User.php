869b8d27a1901551ae2131774f2e65b782361a44
ShokirjonMk <mkshokirjon@gmail.com>
2023-09-11 10:51:59 +0500

    Update User.php

--- api/resources/User.php
+++ api/resources/User.php
@@ -1097,4 +1097,15 @@ class User extends CommonUser
             return false;
         }
     }
+
+    public function beforeSave($insert)
+    {
+        if ($insert) {
+            $this->created_by = Current_user_id();
+        } else {
+            $this->updated_by = Current_user_id();
+        }
+
+        return parent::beforeSave($insert);
+    }
 }

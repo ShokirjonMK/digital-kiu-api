442ebc25cb32c0128916a4e90d837686ab15b1c2
ShokirjonMk <mkshokirjon@gmail.com>
2023-09-11 16:48:02 +0500

    Update Attend.php

--- common/models/model/Attend.php
+++ common/models/model/Attend.php
@@ -370,7 +370,7 @@ class Attend extends \yii\db\ActiveRecord
     public static function createItem($model, $post)
     {
 
-        sleep(300);
+        sleep(1);
 
         $transaction = Yii::$app->db->beginTransaction();
         $errors = [];

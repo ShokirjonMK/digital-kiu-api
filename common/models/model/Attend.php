7d40d6aa5ca1181878a58a6db57b859dd45a5b90
ShokirjonMk <mkshokirjon@gmail.com>
2023-09-12 17:55:27 +0500

    Update Attend.php

--- common/models/model/Attend.php
+++ common/models/model/Attend.php
@@ -522,13 +522,13 @@ class Attend extends \yii\db\ActiveRecord
         //     return simplify_errors($errors);
         // }
 
-        if (!in_array($model->edu_plan_id, [
-            55, 131
-        ])) {
-            $errors[] = ["Ruxsat berilmagan"];
-            $transaction->rollBack();
-            return simplify_errors($errors);
-        }
+        // if (!in_array($model->edu_plan_id, [
+        //     55, 131
+        // ])) {
+        //     $errors[] = ["Ruxsat berilmagan"];
+        //     $transaction->rollBack();
+        //     return simplify_errors($errors);
+        // }
 
         if ($model->save()) {
 

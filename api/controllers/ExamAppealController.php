5de6f5b4903d9ad8510f8d9b264822dd2bd7908a
ShokirjonMk <mkshokirjon@gmail.com>
2023-09-22 15:15:04 +0500

    Update ExamAppealController.php

--- api/controllers/ExamAppealController.php
+++ api/controllers/ExamAppealController.php
@@ -95,43 +95,66 @@ class ExamAppealController extends ApiActiveController
         //         $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 40 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 60');
         //     }
         // }
-        $ball_diff = Yii::$app->request->get('ball_diff');
+
+
+        // $ball_diff = Yii::$app->request->get('ball_diff');
 
         // Check if the 'queryfilter' variable is set. If it is, proceed with further filtering based on 'ball_diff'.
+        // if (isset($queryfilter)) {
+
+        //     // Use a switch statement to handle multiple conditions based on the value of 'ball_diff'.
+        //     switch ($ball_diff) {
+        //         case 'diff_less_than_5':
+        //             // Filter for cases where the absolute difference between old_ball and ball is less than or equal to 5 and greater than 0.
+        //             $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 0 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 5');
+        //             break;
+
+        //         case 'diff_6_to_10':
+        //             // Filter for cases where the absolute difference between old_ball and ball is between 6 and 10.
+        //             $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 5 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 10');
+        //             break;
+
+        //         case 'diff_11_to_20':
+        //             // Filter for cases where the absolute difference between old_ball and ball is between 11 and 20.
+        //             $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 10 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 20');
+        //             break;
+
+        //         case 'diff_21_to_40':
+        //             // Filter for cases where the absolute difference between old_ball and ball is between 21 and 40.
+        //             $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 20 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 40');
+        //             break;
+
+        //         case 'diff_41_to_60':
+        //             // Filter for cases where the absolute difference between old_ball and ball is between 41 and 60.
+        //             $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 40 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 60');
+        //             break;
+
+        //         default:
+        //             // Optionally, handle other cases or do nothing.
+        //             break;
+        //     }
+        // }
+
+        $ball_diff = Yii::$app->request->get('ball_diff');
+
+        // Define an associative array mapping ball_diff keys to SQL conditions
+        $conditionMap = [
+            'diff_less_than_5' => 'ABS(exam_appeal.old_ball - exam_appeal.ball) > 0 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 5',
+            'diff_6_to_10'     => 'ABS(exam_appeal.old_ball - exam_appeal.ball) > 5 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 10',
+            'diff_11_to_20'    => 'ABS(exam_appeal.old_ball - exam_appeal.ball) > 10 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 20',
+            'diff_21_to_40'    => 'ABS(exam_appeal.old_ball - exam_appeal.ball) > 20 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 40',
+            'diff_41_to_60'    => 'ABS(exam_appeal.old_ball - exam_appeal.ball) > 40 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 60',
+        ];
+
+        // Check if queryfilter is set. If yes, proceed with further filtering.
         if (isset($queryfilter)) {
-
-            // Use a switch statement to handle multiple conditions based on the value of 'ball_diff'.
-            switch ($ball_diff) {
-                case 'diff_less_than_5':
-                    // Filter for cases where the absolute difference between old_ball and ball is less than or equal to 5 and greater than 0.
-                    $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 0 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 5');
-                    break;
-
-                case 'diff_6_to_10':
-                    // Filter for cases where the absolute difference between old_ball and ball is between 6 and 10.
-                    $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 5 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 10');
-                    break;
-
-                case 'diff_11_to_20':
-                    // Filter for cases where the absolute difference between old_ball and ball is between 11 and 20.
-                    $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 10 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 20');
-                    break;
-
-                case 'diff_21_to_40':
-                    // Filter for cases where the absolute difference between old_ball and ball is between 21 and 40.
-                    $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 20 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 40');
-                    break;
-
-                case 'diff_41_to_60':
-                    // Filter for cases where the absolute difference between old_ball and ball is between 41 and 60.
-                    $query->andWhere('ABS(exam_appeal.old_ball - exam_appeal.ball) > 40 AND ABS(exam_appeal.old_ball - exam_appeal.ball) <= 60');
-                    break;
-
-                default:
-                    // Optionally, handle other cases or do nothing.
-                    break;
+            // Check if the current ball_diff key exists in the condition map.
+            if (isset($conditionMap[$ball_diff])) {
+                // Add the SQL snippet to the query's WHERE clause.
+                $query->andWhere($conditionMap[$ball_diff]);
             }
         }
+
 
 
         if (isRole("teacher")) {
